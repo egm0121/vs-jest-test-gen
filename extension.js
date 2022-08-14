4f90cf5bb2803e87e82dad92207f432b96558ca2
Giulio Dellorbo <giulio.dellorbo@8x8.com>
2022-08-14 11:26:38 -0400

    Initial release

---
+++ extension.js
@@ -0,0 +1,46 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+const vscode = require('vscode');
+const run = require('jest-test-gen').run;
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+function activate(context) {
+
+	// Use the console to output diagnostic information (console.log) and errors (console.error)
+	// This line of code will only be executed once when your extension is activated
+	console.log('Congratulations, your extension "vs-jest-test-gen" is now active!');
+
+	// The command has been defined in the package.json file
+	// Now provide the implementation of the command with  registerCommand
+	// The commandId parameter must match the command field in package.json
+	let disposable = vscode.commands.registerCommand('vs-jest-test-gen.generateForCurrentFile', function () {
+		// The code you place here will be executed every time your command is executed
+		if (!vscode.window.activeTextEditor) return;
+		const activeDocument = vscode.window.activeTextEditor.document;
+		if (activeDocument.uri.scheme !== 'file' ) return;
+		let testFilename = '';
+		try {
+			testFilename = run({ _: [activeDocument.fileName] });
+		} catch (err) {
+			vscode.window.showErrorMessage('Failed to generate tests :(', [err.toString()]);
+		}
+		console.log('generated test file: ', testFilename);
+		// Display a message box to the user
+		vscode.window.showInformationMessage('Tests generated successfully!');
+	});
+
+	context.subscriptions.push(disposable);
+}
+
+// this method is called when your extension is deactivated
+function deactivate() {}
+
+module.exports = {
+	activate,
+	deactivate
+}
